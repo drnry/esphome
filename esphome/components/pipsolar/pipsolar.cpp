5020154372856f38bba5822a97a7a147357f4a6b
drnry <99051657+drnry@users.noreply.github.com>
2025-04-05 14:21:48 +0200

    Update volt_float, volt_cutoff in pipsolar.cpp

--- esphome/components/pipsolar/pipsolar.cpp
+++ esphome/components/pipsolar/pipsolar.cpp
@@ -35,11 +35,11 @@ void Pipsolar::bms_update(float f) {
   int warning = 0;
   int fc = 0;  // force charge
   int volt_cv = this->bms_chargevoltage_->raw_state * 10;
-  int volt_float = 525;
+  int volt_float = 570;
   int cur_max_ch = this->bms_chargelimit_->raw_state * 10;
   int stopdis = this->bms_stopdischarge_->raw_state;
   int stopch = this->bms_stopcharge_->raw_state;
-  int volt_cutoff = 450;
+  int volt_cutoff = 430;
   int cur_max_dis = this->bms_dischargelimit_->raw_state * 10;
 
   char bms_msg[127];
@@ -61,7 +61,7 @@ void Pipsolar::eminfo_update(float f) {
     ESP_LOGD(TAG, "eminfo Sensor NaN");
     return;
   }
-  int maxfeed = 10000;  // TODO: add sensor
+  int maxfeed = 12000;  // TODO: add sensor
   int dir = f > 0 ? 1 : 0;
   char eminfo_msg[127];
   snprintf(eminfo_msg, sizeof(eminfo_msg), "^S026EMINFO00000,%05i,%01i,%05i", maxfeed, dir, abs((int) f));
